b3b69fba88e4fa8f1a8ad5e7637e81df3ac39687
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2023-07-30 15:33:25 +0200

    Add more functionality and visual changes

--- javascript.js
+++ javascript.js
@@ -6,35 +6,55 @@ let numberOfRounds = 0;
 let playerChoice = null;
 let ties = 0;
 
+function choices(){
+    console.log("player choice is " + playerChoice);
+    console.log("computer choice is " + computerChoice);
+}
+
 // Rock button
 const rockbtn = document.getElementById("rock-btn");
 rockbtn.addEventListener('click', function handleClick(event) {
 playerChoice = "rock";
+document.getElementById("player-choice-container").style.transform = "rotateY(0)";
+document.getElementById("computer-choice-container").style.transform = "rotateY(180deg)";
+document.getElementById("player-choice-img").src="images/150_rock.png";
 playRound();
 });
 // Paper button
 const paperbtn = document.getElementById("paper-btn");
 paperbtn.addEventListener('click', function handleClick(event) {
 playerChoice = "paper";
+document.getElementById("player-choice-container").style.transform = "rotateY(0)";
+document.getElementById("computer-choice-container").style.transform = "rotateY(180deg)";
+document.getElementById("player-choice-img").src="images/150_paper.png";
 playRound();
 });
 // Scissors button
 const scissorsbtn = document.getElementById("scissors-btn");
 scissorsbtn.addEventListener('click', function handleClick(event) {
 playerChoice = "scissors";
+document.getElementById("player-choice-container").style.transform = "rotateY(0)";
+document.getElementById("computer-choice-container").style.transform = "rotateY(180deg)";
+document.getElementById("player-choice-img").src="images/150_scissors.png";
 playRound();
 });
 
 // Returns computers choice
 function getComputerChoice (){  
     const computerChoice = Math.floor(Math.random() * 3 + 1);
-        if (computerChoice===1){
+        if (computerChoice === 1){
+            document.getElementById("computer-choice-img").src="images/150_rock.png";
+            console.log("computer choice is " + computerChoice);
             return "rock";
         }
-        else if (computerChoice===2){
+        else if (computerChoice === 2){
+            document.getElementById("computer-choice-img").src="images/150_paper.png";
+            console.log("computer choice is " + computerChoice);
             return "paper";
         }
         else {
+            document.getElementById("computer-choice-img").src="images/150_scissors.png";
+            console.log("computer choice is " + computerChoice);
             return "scissors";
         }
     }
@@ -43,6 +63,7 @@ function getComputerChoice (){
 function playRound(){
     const player = playerChoice;
     const computer = getComputerChoice ();
+    console.log("computer choice inside the function is " + computer);
     numberOfRounds++;
     document.getElementById("num-of-rounds").innerHTML = numberOfRounds;
 
@@ -79,10 +100,31 @@ function playRound(){
 
 function checkRound(){
     if (playerScore === 5){
-        alert ("The human race is saved, you won!");
+        document.getElementById("end-game-message").innerHTML = "Congratulations!<br> You won!";
+        document.getElementById("end-game-message").style.color = "lime";
+        document.getElementById("overlay").style.opacity = "1";
+        document.getElementById("overlay").style.backgroundImage = "url('victory_image.jpg')";
+        document.getElementById("overlay").style.backgroundSize = "cover";
+        document.getElementById("overlay").style.backgroundAttachment = "fixed";
+        document.getElementById("overlay").style.backgroundRepeat = "no-repeat";
+        document.getElementById("btn-restart").style.boxShadow = "0 0 .2rem #00fff2, 0 0 .1rem #00fff2, 0 0 1rem #00fff2, 0 0 0.4rem #00fff2, 0 0 1.4rem #00fff2, inset 0 0 0.7rem #00fff2";
+        document.getElementById("btn-restart").style.animation = "restartBtnHoverAngel 0.3s forwards";
+        document.getElementById("restartContainer").style.boxShadow = "0 0 .2rem #00fff2, 0 0 .1rem #00fff2, 0 0 1rem #00fff2, 0 0 0.4rem #00fff2, 0 0 1.4rem #00fff2, inset 0 0 0.7rem #00fff2";
+        openRestartContainer();
     }
     else if (computerScore === 5){
-        alert("You lost, human race is doomed..");       
+        document.getElementById("end-game-message").innerHTML = "Game over!<br>You lost!";
+        document.getElementById("end-game-message").style.color = "red";
+        document.getElementById("overlay").style.opacity = "1";
+        document.getElementById("overlay").style.backgroundImage = "url('thresh.jpg')";
+        document.getElementById("overlay").style.backgroundSize = "cover";
+        document.getElementById("overlay").style.backgroundAttachment = "fixed";
+        document.getElementById("overlay").style.backgroundRepeat = "no-repeat";
+        document.getElementById("overlay").style.backgroundPosition = "center center";
+        document.getElementById("btn-restart").style.boxShadow = "0 0 .2rem #ff0000, 0 0 .1rem #ff0000, 0 0 1rem #ff0000, 0 0 0.4rem #ff0000, 0 0 1.4rem #ff0000, inset 0 0 0.7rem #ff0000;";
+        document.getElementById("btn-restart").style.animation = "restartBtnHover 0.3s forwards";
+        document.getElementById("restartContainer").style.boxShadow = "0 0 .2rem #ff0000, 0 0 .1rem #ff0000, 0 0 1rem #ff0000, 0 0 0.4rem #ff0000, 0 0 1.4rem #ff0000, inset 0 0 0.7rem #ff0000";
+        openRestartContainer();   
     }
     else {
         return;
@@ -101,6 +143,27 @@ function resetGame(){
     playerChoice = null;
     ties = 0;
 }
+
+function openRestartContainer() {
+    
+    document.getElementById("restartContainer").style.transform = "translate(-50%, -50%) scale(1)";
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function closeRestartContainer() {
+    document.getElementById("restartContainer").style.transform = "translate(-50%, -50%) scale(0)";
+    document.getElementById("overlay").style.display = "none";
+  }
+
+//   Restart game button
+
+    const restartbtn = document.getElementById("btn-restart");
+    restartbtn.addEventListener('click', function handleClick(event) {
+        closeRestartContainer();
+        resetGame();
+        document.getElementById("player-choice-container").style.transform = "rotateY(90deg)";
+        document.getElementById("computer-choice-container").style.transform = "rotateY(90deg)";
+    });
 
 function stats(){
     console.log(playerScore);
